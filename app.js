d63cc9ee63eac8a0881624746814f1b03530dd98
zawminsoe <alinyaung225800@gmail.com>
2024-03-30 22:39:12 +0630

    Fist released

--- app.js
+++ app.js
@@ -14,44 +14,45 @@ let getMovie = () => {
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
-        console.log(data);
-        console.log(data.Poster);
-        console.log(data.Title);
-        console.log(data.Poster);
-        console.log(data.Rated);
-        console.log(data.Year);
-        console.log(data.Runtime);
-        console.log(data.Genre);
-        console.log(data.Plot);
-        console.log(data.Actors);
+        console.log(data.Response)
+        //if movie exists in database
+        if (data.Response == "True") {
+          result.innerHTML = `
+<div class="info">
+<img src=${data.Poster} class= "poster"/>
+<div>
+<h2>${data.Title}</h2>
+<div class="rating">
+<p>⭐</p>
+  <h4>${data.imdbRating}</h4>
+</div>
+<div class="details">
+<span>${data.Rated}</span>
+<span>${data.Year}</span>
+<span>${data.Runtime}</span>
+</div>
+<div class="genre">
+<div>${data.Genre.split(",").join("</div><div>")}</div>
 
-        result.innerHTML = `
-      <div class="info">
-      <img src=${data.Poster} class= "poster"/>
-      <div>
-      <h2>${data.Title}</h2>
-      <div class="rating">
-      <p>⭐</p>
-        <h4>${data.imdbRating}</h4>
-      </div>
-      <div class="details">
-      <span>${data.Rated}</span>
-      <span>${data.Year}</span>
-      <span>${data.Runtime}</span>
-      </div>
-      <div class="genre">
-      <div>${data.Genre.split(",").join("</div><div>")}</div>
+</div>
+</div>
+</div>
+<h3>Plot:</h3>
+<p>${data.Plot}</p>
+<h3>Cast:</h3>
+<p>${data.Actors}</p>
 
-      </div>
-      </div>
-      </div>
-      <h3>Plot:</h3>
-      <p>${data.Plot}</p>
-      <h3>Cast:</h3>
-      <p>${data.Actors}</p>
-
-      `;
-      });
+`;
+        }
+        // If movie deosnot exists in data base
+        else{
+          result.innerHTML = `<h3 class="msg">${data.Error}</h3>`
+        }
+        
+      })
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">Error Occured</h3>`
+      })
   }
 };
 window.addEventListener("load", getMovie);
